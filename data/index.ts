648487e66c32ee7dd5ffafe63bc4254b47bb62af
KalpeshBhoir <kalpeshbhoir2000@gmail.com>
2024-07-08 15:32:48 +0530

    added information

--- data/index.ts
+++ data/index.ts
@@ -72,11 +72,11 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
+    title: "Kidney Disease classification Deep Learning Project",
+    des: "Implemented Deep Learning Techniques along with MLflow and DVC for experiment tracking and version control , respectively",
+    img: "/k.png",
+    iconLists: [],
+    link: "https://github.com/BhoirKalpesh/Kidney-Disease-Classification-Deep-Learning-Project",
   },
   {
     id: 2,
@@ -84,24 +84,17 @@ export const projects = [
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
     img: "/p2.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
+    link: "https://video-conference-app-ashen.vercel.app/",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
+    title: "Big Mart Sales Prediction",
+    des: "Gathered data from kaggle, conducted data preprocessing, feature engineering, selection and resolved data quality issues like duplicates and missing values",
+    img: "/bigmart1.png",
+    iconLists: [],
+    link: "https://github.com/BhoirKalpesh/Big_Mart_Sales_Prediction",
   },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
-  },
+
 ];
 
 export const testimonials = [
@@ -173,31 +166,24 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Machine Learning and CyberSecurity Intern",
+    desc: "Implemented models for image classification and real-time object detection,built chatbots using NLP techniques, and done hands-on experience in cyber security tools",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Machine Learning Intern",
+    desc: "Developed and implemented foundational machine learning models for various applications.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Web Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
-  },
-  {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
-    className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
   },
 ];
 
