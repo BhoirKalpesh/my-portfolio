b38f48653d757889cda838daec765ab0cc015c9c
KalpeshBhoir <kalpeshbhoir2000@gmail.com>
2024-07-09 00:12:03 +0530

    client section

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ const Home = () => {
         <Hero />
         <Grid />
         <RecentProjects />
-        <Clients />
+        
         <Experience />
         <Approach />
         <Footer />
--- data/index.ts
+++ data/index.ts
@@ -49,11 +49,11 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently working on Deep Learning project",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-    titleClassName: "justify-center md:justify-start lg:justify-center",
+    titleClassName: "justify-center md:justify-start  lg:justify-center",
     img: "/b5.svg",
     spareImg: "/grid.svg",
   },
